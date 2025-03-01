612535dd94c37880ad0f27d10725d74f2672f167
mit2208 <shethmeet24@gmail.com>
2025-03-01 15:02:14 +0530

    commit

--- my-express-app/Backend/Backend.js
+++ my-express-app/Backend/Backend.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const mysql = require('mysql');
 require('dotenv').config();
 
 const app = express();
